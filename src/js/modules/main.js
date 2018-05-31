4db9ff52e351aee35a14e50bdb45908571755917
kondr1996 <kondrashkin403@mail.ru>
2018-05-31 22:11:34 +0300

    mobile design

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -23,11 +23,7 @@ $(function() {
         $('.owl_items-num-inherit').text('/' + e.item.count);
     })
     
-    // $(".parallax-rewiews").paroller({ factor: '0.1', type: 'foreground', direction: 'vertical' });
-    // $(".parallax-about-us").paroller({ factor: '0.3', type: 'foreground', direction: 'vertical' });
-    // $(".parallax-offer").paroller({ factor: '0.2', type: 'foreground', direction: 'vertical' });
-    // $(".parallax-project").paroller({ factor: '0.2', type: 'foreground', direction: 'vertical' });
-    // //animation on main block spiderweb
+  // //animation on main block spiderweb
     particlesJS('particles-js',
   
     {
